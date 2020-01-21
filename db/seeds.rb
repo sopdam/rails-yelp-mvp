e5e194f4264710ada033141cccafbeac9b72aaa8
Stephanie Opdam <scm.opdam@gmail.com>
2020-01-21 20:57:56 +0000

    Finished backend except for admin - still needs frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([
+  {
+    name: 'Sushi Samba',
+    address: '110 Bishopsgate, London EC2N 4AY',
+    phone_number: '020 3640 7330',
+    category: 'japanese'
+  },
+  {
+    name: 'Sticks n Sushi',
+    address: '11 Henrietta St, Covent Garden, London WC2E 8PY',
+    phone_number: '020 3141 8810',
+    category: 'japanese'
+  },
+  {
+    name: 'Franco Manca',
+    address: '52 Broadway Market, London E8 4QJ',
+    phone_number: '020 7254 7249',
+    category: 'italian'
+  },
+  {
+    name: 'Sichuan Folk',
+    address: '32 Hanbury St, Spitalfields, London E1 6QR',
+    phone_number: '020 7247 4735',
+    category: 'chinese'
+  },
+  {
+    name: 'Belgo',
+    address: '50 Earlham St, London WC2H 9LJ',
+    phone_number: '020 7813 2233',
+    category: 'belgian'
+  }])
